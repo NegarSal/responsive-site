03ffa4256d7bfe074dc7a97990442239b01f174d
NegarSal <Negarsaljooghi@gmail.com>
2021-03-16 20:52:15 +0330

    first commit

---
+++ pro6.js
@@ -0,0 +1,26 @@
+﻿window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 7520|| document.documentElement.scrollTop > 7520) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+$('.thumbnail').click(function(){
+    $('.modal-body').empty();
+    $($(this).parents('div').html()).appendTo('.modal-body');
+    $('#modal').modal({show:true});
+});
+
+$('#modal').on('show.bs.modal', function () {
+    $('.col-6,.row .thumbnail').addClass('blur');
+})
+
+$('#modal').on('hide.bs.modal', function () {
+    $('.col-6,.row .thumbnail').removeClass('blur');
+})
